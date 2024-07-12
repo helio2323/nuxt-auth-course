540edd007e59ca50ab305d9469c35a24555808d7
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2024-07-11 21:57:54 -0300

    11/07

--- nuxt-auth/nuxt.config.ts
+++ nuxt-auth/nuxt.config.ts
@@ -2,9 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase"],
+  modules: ["@nuxtjs/supabase", "@sidebase/nuxt-auth"],
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY
-  }
+  },
+  auth: {
+    provider: {
+      type: 'authjs'
+    },
+    globalAppMiddleware: {
+      isEnabled: true
+    },
+    baseURL: `http://localhost:${process.env.PORT || 3000}`
+  },
 })
